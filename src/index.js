1216a97e1db56ad7fe46dc9895a6690dee58fb59
Damien Nelson <dhnisaman@gmail.com>
2022-12-08 13:34:52 -0500

    commit tree f72f4cf8c49e37cf5a2272296d3f14bd7943b0b8

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
+
+function movieById(id){
+    fetch(`http://localhost:3000/movies/${id}`)
+    .then((resp) => resp.json())
+    .then((resp) => useID(resp))
+}
+
+function useID(resp){
+   const title = document.querySelector("section#movieDetails h4")
+   const summery = document.querySelector('section#movieDetails p')
+   title.textContent = resp.title
+   summery.textContent = resp.summary
+}
+
 const init = () => {
-  
+    document.querySelector('form').addEventListener('submit', e =>{
+        e.preventDefault()
+         movieById(e.target[0].value)
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
